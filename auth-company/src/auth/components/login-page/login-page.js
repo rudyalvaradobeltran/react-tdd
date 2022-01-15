6293e34a4379db42791f99c0e4d115fb3cff425a
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2022-01-15 14:39:17 -0300

    Testing login values: email and password

--- auth-company/src/auth/components/login-page/login-page.js
+++ auth-company/src/auth/components/login-page/login-page.js
@@ -1,11 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+export const validateEmail = email => {
+  const regex = /^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/;
+  return regex.test(email);
+}
 
 const LoginPage = () => {
+  const [emailValidationMessage, setEmailValidationMessage] = useState('');
+  const [passwordValidationMessage, setPasswordValidationMessage] = useState('');
+  const [formValues, setFormValues] = useState({ email: '', password: '' });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { email, password } = e.target.elements;
+    if (!email.value) {
+      setEmailValidationMessage('The email is required');
+    }
+    if (!password.value) {
+      setPasswordValidationMessage('The password is required');
+    } 
+  };
+
+  const handleChange = ({ target: { value, name } }) => {
+    setFormValues({ ...formValues, [name]: value });
+  }
+
+  const handleBlurEmail = () => {
+    if (!validateEmail(formValues.email)) {
+      setEmailValidationMessage('The email is invalid');
+    }
+  }
+
   return (
-    <h1>
-      Login page
-    </h1>
-  )
-}
+    <>
+      <h1>Login page</h1>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          label="email"
+          id="email"
+          name="email"
+          helperText={emailValidationMessage}
+          handleChange={handleChange}
+          value={formValues.email}
+          onBlur={handleBlurEmail}
+        />
+        <TextField
+          label="password"
+          id="password"
+          type="password"
+          name="password"
+          helperText={passwordValidationMessage}
+          handleChange={handleChange}
+          value={formValues.password}
+        />
+        <Button type="submit">Send</Button>
+      </form>
+    </>
+  );
+};
 
 export default LoginPage;
--- auth-company/src/auth/components/login-page/login-page.test.js
+++ auth-company/src/auth/components/login-page/login-page.test.js
@@ -1,10 +1,45 @@
 import React from 'react';
-import { screen, render } from '@testing-library/react';
+import { screen, render, fireEvent } from '@testing-library/react';
 import LoginPage from './login-page';
+
+beforeEach(() => render(<LoginPage />));
 
 describe('When login page is mounted', () => {
   it('must display the login title', () => {
-    render(<LoginPage />);
     expect(screen.getByText(/login page/i)).toBeInTheDocument();
+  });
+  it('must have a form with the following fields: email, password and a submit button', () => {
+    expect(screen.getByLabelText(/email/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /send/i })).toBeInTheDocument();
+  });
+});
+
+describe('When the user leaves empty fields and clicks the submit button', () => {
+  it('display required messages as the format: "The [field name] is required"', async () => {
+    expect(screen.queryByText(/the email is required/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/the password is required/i)).not.toBeInTheDocument();
+    fireEvent.click(screen.getByRole('button', { name: /send/i }));
+    expect(screen.getByText(/the email is required/i)).toBeInTheDocument();
+    expect(screen.getByText(/the password is required/i)).toBeInTheDocument();
+  });
+});
+
+describe('When the user fills the fields and clicks the submit button', () => {
+  it('must not display the required messages"', async () => {
+    screen.getByLabelText(/email/i).value = 'John.doe@test.com';
+    screen.getByLabelText(/password/i).value = 'mySecurePassword666';
+    fireEvent.click(screen.getByRole('button', { name: /send/i }));
+    expect(screen.queryByText(/the email is required/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/the password is required/i)).not.toBeInTheDocument();
+  });
+});
+
+describe('when the user fills and blur the email input with invalid email', () => {
+  it('must display a validation message', () => {
+    const emailInput = screen.getByLabelText(/email/i);
+    fireEvent.change(emailInput, { target: { value: 'invalid.email' } });
+    fireEvent.blur(screen.getByLabelText(/email/i));
+    expect(screen.getByText(/the email is invalid/i)).toBeInTheDocument();
   });
 });
