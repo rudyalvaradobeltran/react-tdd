8d9126d957e30710bb4324305c2d0f61bae99039
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2022-01-14 22:45:33 -0300

    Auth company initial

---
+++ auth-company/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import LoginPage from './LoginPage';
+
+const App = () => {
+  return(
+    <LoginPage />
+  )
+}
+export default App;
---
+++ auth-company/src/auth/components/login-page/login-page.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import LoginPage from './login-page';
+
+describe('When login page is mounted', () => {
+  it('must display the login title', () => {
+    render(<LoginPage />);
+    expect(screen.getByText(/login page/i)).toBeInTheDocument();
+  });
+});
